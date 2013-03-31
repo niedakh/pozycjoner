5158354b3ed754d5536e4ed3b4661d72e880b24c
Piotr Szymański <niedakh@gmail.com>
2013-04-01 00:20:39 +0200

    - fetching MPK Wroclaw's vehicle positions works
    - implemented fetching MPK's vehicle positions with a getPosition
    function
    - still need to figure out a sensible data structure to return the
    values and decide which values to return

--- mpk.py
+++ mpk.py
@@ -1,15 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+import parsedatetime as pdt
+from datetime import datetime 
 
 class MPKWroclawPositionier:
     """ TODO:description """
     
     def __init__(self):
         self.mpk_url = 'http://pasazer.mpk.wroc.pl/bus_gps/position'
-        
         self.provider = 'MPKWroclaw'
         self.mpk_list_url = 'http://pasazer.mpk.wroc.pl/jak-jezdzimy/mapa-pozycji-pojazdow'
-        
+        self.dateparser = pdt.Calendar(pdt.Constants())
         
     def getAvailableLines(self):
         session = requests.session()
@@ -24,9 +25,53 @@ class MPKWroclawPositionier:
         # we are therefore searching for li.bus_lines text child element
         # we want it lowercased because that howe the MPK's javascript does this
         return [line.text.lower() for line in mpk_data_tree.find_all("li", "bus_line")]
-    
+
+    def getPosition(self, line_number):
+        lines = []
+        if isinstance(line_number, int):
+            lines.append(str(line_number).lower())
+        elif isinstance(line_number, str):
+            lines.append(line_number.lower())
+        elif isinstance(line_number,list):
+            for i in line_number:
+                lines.append(str(i).lower())
+        
+        # The POST request expects an urlencoded version of
+        # busList[bus][]=line number, like an array in PHP, ex:
+        # busList[bus][]=5&busList[bus][]=6 etc.
+        #
+        # Because the Request API expects a dictionary, we creeate a
+        # dictionary with one key and put a lines array as the value
+        # the Requests API should convert this to a list of key=value pairs
+        
+        payload = {'busList[bus][]': lines}
+        session = requests.session()
+        r = requests.post(self.mpk_url, data=payload)
+                          
+        if (r.status_code != 200):
+            r.raise_for_status()
+            
+        else:
+            # pasazer.mpk.wroc.pl returns empty json's for all requests
+            # TODO: will figure out a sensible parsed version of return data
+            val = {
+                # TODO: figure out how to return the received time - as a a datetime?
+                'received': self.dateparser.parseDateText(r.headers['last-modified']),
+                'json': r.json()
+            }
+            return val
   
 if __name__ == "__main__":
     mpk = MPKWroclawPositionier()
+    lines = mpk.getAvailableLines()
     print("Available lines:")
-    print(mpk.getAvailableLines())
+    print(lines)
+    
+    print("MPK 125:")
+    print(mpk.getPosition(125))
+    
+    print("MPK A:")
+    print(mpk.getPosition('A'))
+    
+    print("MPK all:")
+    print(mpk.getPosition(lines))
